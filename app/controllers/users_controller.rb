330798ec329fdd9f50c8675c8edee1b5e0bed138
itouma <68621609+itouma@users.noreply.github.com>
2020-08-02 23:32:14 +0900

    ユーザー編集機能の実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  
+  def edit
+  end
+  
+  def update
+    # binding.pry
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+  # user = User.find(params[:id])
+  # user.update(user_params)
+  
+  # user = User.find(current_user.id)でも可
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
